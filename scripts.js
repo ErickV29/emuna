2925465ca06ddf24ca4666363eaf3d5e96fa7c9f
ErickV29 <116609491+ErickV29@users.noreply.github.com>
2024-07-15 18:30:36 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('view-profile1', 'view-profile2', 'view-profile3').addEventListener('click', function() {
+    
+    wi
+window.location.href = 'perfil-psicologo1.html','perfil-psicologo1.html','perfil-psicologo3.html'; // Reemplaza con la URL correcta del perfil
+
+});
